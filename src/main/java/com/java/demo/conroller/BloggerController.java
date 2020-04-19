8b77abaa41246c6b92e5da6be8e3b781f010d491
ashi29 <63950436+ashi29@users.noreply.github.com>
2020-04-19 14:46:46 +0530

    Add files via upload

---
+++ src/main/java/com/java/demo/conroller/BloggerController.java
@@ -0,0 +1,17 @@
+package com.java.demo.conroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BloggerController {
+
+
+	@GetMapping("/home")
+	public String home(){
+		
+		return "ashish verma";
+	
+	}
+}
